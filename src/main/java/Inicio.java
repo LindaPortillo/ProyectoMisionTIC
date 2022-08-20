eacfb386380f7f6488e1572aa90c9d3a072319d1
Linda Portillo <linda.portillov@gmail.com>
2022-08-20 10:58:52 -0500

    Segundo Commit de nuestro proyecto

--- src/main/java/Inicio.java
+++ src/main/java/Inicio.java
@@ -1,5 +1,7 @@
 public class Inicio {
     public static void main (String[] args) {
         System.out.println("Hola Aprendices Fuera de Linea");
+        System.out.println("Este es nuestro primer sprint");
+        System.out.println("La intención es que con ayuda de Git tengamos siempre nuestros códigos asegurados");
     }
 }
